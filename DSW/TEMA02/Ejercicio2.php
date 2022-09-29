4fe70ad86c362c1fa05645a1257ac6fdd65de71f
GuillermoSH <siciliahernandezguillermo@gmail.com>
2022-09-29 19:44:46 +0100

    :white_check_mark: Agregado ejercicio 5 y 6

---
+++ DSW/TEMA02/Ejercicio2.php
@@ -0,0 +1,44 @@
+<?php declare(strict_types=1)?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        function sorteo($cantidadNumeros) {
+            $sorteo=[];
+    
+            do {
+                $numeroAleatorio = random_int(1,49);
+    
+                if ((in_array($numeroAleatorio, $sorteo))) {
+                    array_push($sorteo,$numeroAleatorio);
+                }
+            } while (count($sorteo) < $cantidadNumeros);
+    
+            sort($sorteo);
+            return $sorteo;
+        }
+
+        function formatearSorteo() {
+            $sorteo = sorteo(6);
+
+            echo "<table>\n";
+            echo "<tr>\n";
+            echo "<td>Numeros ganadores del sorteo:</td>\n"
+            echo "</tr>\n";
+
+            for($i =0;i<count($sorteo);$i++) {
+                echo "<tr>\n";
+                echo "<td>$sorteo[$i]</td>\n"
+                echo "</tr>\n";
+            }
+            echo "</table>\n"
+        }
+    ?>
+</body>
+</html>
