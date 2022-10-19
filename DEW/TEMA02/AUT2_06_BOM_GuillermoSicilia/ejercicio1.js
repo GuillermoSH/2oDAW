210816ae72e1e5d6b61555f72afbf9583d7025e9
GuillermoSH <siciliahernandezguillermo@gmail.com>
2022-10-19 19:29:40 +0100

    :white_check_mark: Terminado ejercicio 1 AUT2_06 DEW

--- DEW/TEMA02/AUT2_06_BOM_GuillermoSicilia/ejercicio1.js
+++ DEW/TEMA02/AUT2_06_BOM_GuillermoSicilia/ejercicio1.js
@@ -1,7 +1,14 @@
 let newWindow;
 
+/**
+ * "Open a new window with the specified URL, height, width, top and left properties."
+ * 
+ * The first line of the function declares a variable named url and assigns it the value "about:blank".
+ * This is the URL that will be loaded in the new window
+ */
 function openNewWindow() {
-    let url = "https://www.google.es"
+    //let url = "https://www.google.es"
+    let url = "about:blank";
     let height = 400;
     let width = 400;
     let left = (screen.width - width) / 2;
@@ -11,6 +18,9 @@ function openNewWindow() {
     newWindow = window.open(url, "", features);
 }
 
+/**
+ * If the newWindow variable is not null, close it. Otherwise, print a message to the console.
+ */
 function closeNewWindow() {
     if (newWindow != null) {
         newWindow.close();
@@ -19,24 +29,50 @@ function closeNewWindow() {
     }
 }
 
+/**
+ * It enlarges the new window by a certain number of pixels, but only if the new window is smaller than
+ * the screen
+ * @param px - The number of pixels to resize the window by.
+ */
 function enlargeNewWindow(px) {
     let newWidth = newWindow.innerWidth + px;
     let newHeight = newWindow.innerHeight + px;
 
-    if (newWidth < screen.width && newHeight < screen.height) {
-        newWindow.resizeTo(newWidth, newHeight);
+    if (newWidth < screen.availWidth || newHeight < screen.availHeight) {
+        newWindow.resizeBy(px, px);
     } else {
         console.log("No se puede aumentar más la ventana.")
     }
 }
 
+/**
+ * It reduces the size of the window by a given number of pixels
+ * @param px - The number of pixels to resize the window by.
+ */
 function reduceNewWindow(px) {
     let newWidth = newWindow.innerWidth - px;
     let newHeight = newWindow.innerHeight - px;
-    
-    if (newWidth > screen.width && newHeight > screen.height) {
-        newWindow.resizeTo(newWidth, newHeight);
+
+    if (newWidth >= 100 || newHeight >= 59) {
+        newWindow.resizeBy(-px, -px);
     } else {
         console.log("No se puede reducir más la ventana.")
+    }
+}
+
+/**
+ * It moves the new window to the position specified by the user, if the position is within the
+ * screen's boundaries
+ */
+function moveNewWindow() {
+    let xPos = document.getElementById("x").value;
+    let yPos = document.getElementById("y").value;
+    let lesserPosCondition = xPos >= 0 || yPos >= 0;
+    let greaterPosCondition = xPos <= (screen.availWidth - newWindow.innerWidth) || yPos <= (screen.availHeight - newWindow.innerHeight);
+
+    if (lesserPosCondition && greaterPosCondition) {
+        newWindow.moveTo(xPos,yPos);
+    } else {
+        console.log("No se puede mover a esa posición.");
     }
 }
