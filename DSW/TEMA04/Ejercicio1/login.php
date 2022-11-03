b2f0bd4037ba054e0a8fa3b9debb88da4815be1d
GuillermoSH <siciliahernandezguillermo@gmail.com>
2022-11-03 16:11:15 +0000

    :white_check_mark: Terminados los ejercicios de media query

---
+++ DSW/TEMA04/Ejercicio1/login.php
@@ -0,0 +1,45 @@
+<?php declare(strict_types=1); ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+
+<body>
+    <form action="" method="post" enctype="multipart/form-data">
+        <label for="user">Usuario: </label><br>
+        <input type="text" id="user" name="user" /> <br>
+        <label for="pass">Contraseña: </label><br>
+        <input type="password" id="pass" name="pass" /> <br>
+        <button type="submit" value="Entrar" name="entrar">Login</button><br>
+    </form>
+    <?php
+    function verificarCuenta(string $usuariojson, string $username, string $password)
+    {
+        $usuariof = json_decode($usuariojson, true);
+        if ($username == $usuariof['user']) {
+            return password_verify($password, $usuariof['pass']);
+        }
+    }
+    if (isset($_POST["entrar"])) {
+        $BDatos = "/var/www/phpdata/BDusuario.txt";
+        $fdbaseDatos = fopen($BDatos, "r");
+        $header = fgets($fdbaseDatos);
+        $usuario = $_POST['user'];
+        $passw = $_POST['pass'];
+        while ($linea = fgets($fdbaseDatos)) {
+            if (verificarCuenta($linea, $usuario, $passw)) {
+                echo "<script> alert('Bienvenido ", $usuario, "') </script>";
+                exit;
+            }
+        }
+        echo "<script> alert('Error. Usuario/contraseña erróneos') </script>";
+        fclose($fdbaseDatos);
+    }
+    ?>
+</body>
+</html>
--- DSW/TEMA04/Ejercicio1/register.php
+++ DSW/TEMA04/Ejercicio1/register.php
@@ -5,44 +5,27 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Register</title>
 </head>
 
 <body>
-    <form action="Ejercicio1.php" method="post" enctype="multipart/form-data">
-        <table>
-            <tr>
-                <td>
-                    <label for="nombre">Nombre: </label><input type="text" id="nombre" />
-                </td>
-                <td>
-                    <label for="ap1">Apellidos: </label><input type="text" id="ap1" />
-                </td>
-                <td>
-                    <input type="text" id="ap2" />
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <label for="user">Usuario: </label><input type="text" id="juanlo" />
-                </td>
-                <td>
-                    <label for="email">Email: </label><input type="text" id="email" />
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <label for="pass">Contraseña: </label><input type="password" id="pass" />
-                </td>
-                <td>
-                    <label for="passConfirm">Confirmar contraseña: </label>
-                    <input type="password" id="passConfirm" />
-                </td>
-                <td>
-                    <button type="submit" value="Enviar" name="enviar">Enviar</button>
-                </td>
-            </tr>
-        </table>
+    <form action="" method="post" enctype="multipart/form-data">
+        <label for="nombre">Nombre: </label><br>
+        <input required="required" type="text" id="nombre" name="nombre" /><br>
+        <label for="ap1">Apellidos 1: </label><br>
+        <input required="required" type="text" id="ap1" name="ap1" /> <br>
+        <label for="ap2">Apellidos 2: </label><br>
+        <input type="text" id="ap2" name="ap2" /> <br>
+        <label for="user">Usuario: </label><br>
+        <input required="required" type="text" id="user" name="user" /> <br>
+        <label for="email">Email: </label><br>
+        <input required="required" type="text" id="email" name="email" /> <br>
+        <label for="pass">Contraseña: </label><br>
+        <input required="required" type="password" id="pass" name="pass" /> <br>
+        <label for="passConfirm">Confirmar contraseña: </label><br>
+        <input required="required" type="password" id="passConfirm" name="passConfirm" /> <br>
+        <button type="submit" value="Enviar" name="enviar">Registrarse</button><br><br>
+        <a href="login.php">Ir al Login</a>
     </form>
     <?php
     function verifyExistingUser(string $usuariojson, array $registro)
@@ -52,7 +35,7 @@
         return $usuariof['user'] == $registro['user'];
     }
 
-    if (isset($_POST["Enviar"])) {
+    if (isset($_POST["enviar"])) {
         $nombre = $_POST['nombre'];
         $ap1 = $_POST['ap1'];
         $ap2 = $_POST['ap2'];
@@ -64,12 +47,14 @@
         if ($pass != $passConfirm) {
             echo "<script> alert('Error01:\\n\\nLas contraseñas no coinciden.'); </script>";
             exit;
+        } else {
+            $passHash = password_hash($_POST['pass'], PASSWORD_DEFAULT);
+            $usuario = [
+                "nombre" => $nombre, "ap1" => $ap1, "ap2" => $ap2,
+                "user" => $user, "pass" => $passHash, "email" => $email
+            ];
         }
 
-        $usuario = [
-            "nombre" => $nombre, "ap1" => $ap1, "ap2" => $ap2,
-            "user" => $user, "pass" => $pass, "email" => $email
-        ];
         $bbdd = "/var/www/phpdata/BDusuario.txt";
         $fdbaseDatos = fopen($bbdd, "c+");
         $header = fgets($fdbaseDatos);
@@ -87,6 +72,8 @@
             }
             fwrite($fdbaseDatos, $usuarioJson);
             echo "<script> alert('Se ha registrado con éxito'); </script>";
+            fclose($fdbaseDatos);
+            header('Location: login.php');
         }
     }
     ?>
