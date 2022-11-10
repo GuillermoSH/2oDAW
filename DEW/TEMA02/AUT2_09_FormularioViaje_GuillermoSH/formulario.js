c93ed3b36407fafc3da1a27d9a5e64cc8f3da3df
GuillermoSH <siciliahernandezguillermo@gmail.com>
2022-11-10 12:30:34 +0000

    :construction: Avance en formulario viaje

--- DEW/TEMA02/AUT2_09_FormularioViaje_GuillermoSH/formulario.js
+++ DEW/TEMA02/AUT2_09_FormularioViaje_GuillermoSH/formulario.js
@@ -1,4 +1,11 @@
-function validation(element) {
+/**
+ * It checks if the name or surname field contains digits or is empty. If it does, it sets the border
+ * to red, otherwise it sets it to yellowgreen.
+ *
+ * @param element - the element to be validated
+ * @returns a boolean value.
+ */
+function nameSurnameValidation(element) {
     const noDigits = /\d/;
 
     if (element == "name") {
@@ -29,9 +36,9 @@ function nifNieValidation() {
             return false;
         }
     } else if (nieRegex.test(nifNie.value)) {
-        let valurNifNie = nifNie.value.replace("X", 0).replace("Y", 1).replace("Z", 2);
-        console.log(valurNifNie);
-        if (letterCheck(valurNifNie)) {
+        let valueNifNie = nifNie.value.replace("X", 0).replace("Y", 1).replace("Z", 2);
+        console.log(valueNifNie);
+        if (letterCheck(valueNifNie)) {
             nifNie.setAttribute("style", "border: 2px solid yellowgreen;");
             return true;
         } else {
@@ -44,6 +51,12 @@ function nifNieValidation() {
     }
 }
 
+/**
+ * It takes the first 8 digits of the DNI, divides them by 23, and then checks if the remainder matches
+ * the last digit of the DNI.
+ * @param dni - The DNI number to check.
+ * @returns a boolean value.
+ */
 function letterCheck(dni) {
     const validLetters = 'TRWAGMYFPDXBNJZSQVHLCKET';
     let letter = dni.substr(-1);
@@ -64,22 +77,132 @@ function postalCodeValidation() {
     return true;
 }
 
+/**
+ * If the phone number is not empty and matches the regex, then it's valid
+ * 
+ * @param phoneType - The type of phone number you want to validate.
+ * @returns a boolean value.
+ */
 function phoneValidation(phoneType) {
     let regex;
-
+    
     if (phoneType == "telephone") {
         phoneType = document.getElementById("in_telephone");
-        regex =  /^(\+\d{2})[ -]?[0-9]{9}|^9[0-9]{8}$/;
+        if (phoneType.value.substr(0,3) == "+34" || phoneType.value.substr(0,4) == "0034") {
+            regex =  /^(\+34|0034)[ -]?9\d{8}|^9\d{8}$/; // internacional
+        } else {
+            regex = /^9\d{8}$/; // nacional
+        }
     } else {
         phoneType = document.getElementById("in_mobilephone");
-        regex = /^\(\+\d{2,3}\)|^[6-8][0-9]{8}$/;
+        regex = /^[6-8]\d{8}$/;
     }
-
+    
     if (!regex.test(phoneType.value) || phoneType.value == "") {
         phoneType.setAttribute("style", "border: 2px solid red;");
         return false;
     }
     phoneType.setAttribute("style", "border: 2px solid yellowgreen;");
+    return true;
+}
+
+/**
+ * If the input field is empty, set the border to red, otherwise set it to yellowgreen.
+ * 
+ * @returns a boolean value.
+ */
+function peopleTravelling() {
+    let numPeople = document.getElementById("in_numPeople");
+
+    if (numPeople == "") {
+        numPeople.setAttribute("style", "border: 2px solid red;");
+        return false;
+    }
+    numPeople.setAttribute("style", "border: 2px solid yellowgreen;");
+    return true;
+}
+
+/**
+ * If the input value is not a valid date, then set the border to red, otherwise set it to yellowgreen.
+ * 
+ * @returns a boolean value.
+ */
+function dateValidation() {
+    const regex = /^\d{2}\/\d{2}\/\d{4}$/;
+    let date = document.getElementById("in_date");
+
+    if (!regex.test(date.value) || date.value == "") {
+        date.setAttribute("style", "border: 2px solid red;");
+        return false;
+    }
+    date.setAttribute("style", "border: 2px solid yellowgreen;");
+    return true;
+}
+
+function emailValidation() {
+    const regex = /^\w+([.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    let email = document.getElementById("in_email");
+
+    if (!regex.test(email.value) || email.value == "") {
+        email.setAttribute("style", "border: 2px solid red;");
+        return false;
+    }
+    email.setAttribute("style", "border: 2px solid yellowgreen;");
+    return true;
+}
+
+/**
+ * If the input value is not a valid IPv4 or IPv6 address, or if the input value is empty, then the
+ * input field will be highlighted in red. Otherwise, the input field will be highlighted in green.
+ * 
+ * @returns a boolean value.
+ */
+function ipv4Ipv6Validation() {
+    const regex = /((^\s*(((\d|[1-9]\d|1\d{2}|2[0-4]\d|25[0-5])\.){3}(\d|[1-9]\d|1\d{2}|2[0-4]\d|25[0-5]))\s*$)|(^\s*((([\dA-Fa-f]{1,4}:){7}([\dA-Fa-f]{1,4}|:))|(([\dA-Fa-f]{1,4}:){6}(:[\dA-Fa-f]{1,4}|((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([\dA-Fa-f]{1,4}:){5}(((:[\dA-Fa-f]{1,4}){1,2})|:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([\dA-Fa-f]{1,4}:){4}(((:[\dA-Fa-f]{1,4}){1,3})|((:[\dA-Fa-f]{1,4})?:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([\dA-Fa-f]{1,4}:){3}(((:[\dA-Fa-f]{1,4}){1,4})|((:[\dA-Fa-f]{1,4}){0,2}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([\dA-Fa-f]{1,4}:){2}(((:[\dA-Fa-f]{1,4}){1,5})|((:[\dA-Fa-f]{1,4}){0,3}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([\dA-Fa-f]{1,4}:){1}(((:[\dA-Fa-f]{1,4}){1,6})|((:[\dA-Fa-f]{1,4}){0,4}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(:(((:[\dA-Fa-f]{1,4}){1,7})|((:[\dA-Fa-f]{1,4}){0,5}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:)))(%.+)?\s*$))/gm;
+    let ip = document.getElementById("in_ip");
+
+    if (!regex.test(ip.value) || ip.value == "") {
+        ip.setAttribute("style","border: 2px solid red;");
+        return false;
+    }
+    ip.setAttribute("style","border: 2px solid yellowgreen");
+    return true;
+}
+
+/**
+ * If the value of instagram or twitter field is empty or does not match the regular expression, then 
+ * the border of the input field will be red. Otherwise, the border will be green.
+ * 
+ * @param socialMediaType - The id of the input field.
+ * @returns a boolean value.
+ */
+function instaTwitterValidation(socialMediaType) {
+    const regex = /^\@[\w_-]+$/;
+    let socialMedia = document.getElementById(socialMediaType);
+    
+    if (!regex.test(socialMedia.value) || socialMedia.value == "") {
+        socialMedia.setAttribute("style","border: 2px solid red;");
+        return false;
+    }
+    socialMedia.setAttribute("style","border: 2px solid yellowgreen");
+    return true;
+}
+
+/**
+ * It checks if the car plate value matches the regex pattern and if it does, it changes the border color
+ * to green, otherwise it changes it to red.
+ * 
+ * @returns a boolean value.
+ */
+function carPlateValidation() {
+    const regex = /^\d{4}\-[A-Z]{3}/;
+    let carPlate = document.getElementById("in_plate");
+
+    if (!regex.test(carPlate.value) || carPlate.value == "") {
+        carPlate.setAttribute("style","border: 2px solid red;");
+        return false;
+    }
+    carPlate.setAttribute("style","border: 2px solid yellowgreen");
     return true;
 }
 
@@ -96,26 +219,28 @@ function showPostalCode() {
 
 function showSocialMedia() {
     let insta = document.getElementById("instagram");
-    let face = document.getElementById("facebook");
+    let twitter = document.getElementById("twitter");
 
     insta.classList.remove("hidden");
-    face.classList.remove("hidden");
+    twitter.classList.remove("hidden");
 }
 
 function showVehicleInfo() {
     let brand = document.getElementById("brand");
     let model = document.getElementById("model");
+    let plate = document.getElementById("plate");
 
     brand.classList.remove("hidden");
     model.classList.remove("hidden");
+    plate.classList.remove("hidden");
 }
 
 function launch() {
     event.preventDefault();
 
-    if (!(validation("name") && validation("surname") && nifNieValidation())) {
+    if (!(validation("name") && validation("surname") && nifNieValidation() && phoneValidation("telephone") && phoneValidation("mobilephone") && peopleTravelling() && postalCodeValidation() && dateValidation() && emailValidation() && ipv4Ipv6Validation())) {
         alert("Campos incompletos o incorrectos");
     } else {
-        alert("Too correcto manito")
+        alert("Too correcto manito");
     }
 }
