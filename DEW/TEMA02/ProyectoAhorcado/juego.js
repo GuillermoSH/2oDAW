2c345293d8c1f847a9c0820410a57584f385d4b1
GuillermoSH <siciliahernandezguillermo@gmail.com>
2022-11-16 12:25:46 +0000

    :construction: Trabajo en proyecto ahorcado

--- DEW/TEMA02/ProyectoAhorcado/juego.js
+++ DEW/TEMA02/ProyectoAhorcado/juego.js
@@ -1,32 +1,55 @@
-const palabras = ["Parque Jurasico", "Star Wars", "Cars","Buscando a Nemo","La Sirenita","El bueno, el feo y el malo"];
+const palabras = ["Parque Jurasico", "Star Wars", "Cars", "Buscando a Nemo", "La Sirenita", "El bueno, el feo y el malo"];
 let palabraAdivinar = palabras[parseInt(Math.random() * palabras.length)];
-let adivinanza = palabraAdivinar.replaceAll(/\w/g,"_");
+let adivinanza = palabraAdivinar.replaceAll(/\w/g, "_");
+let intentos = 6;
 
 document.getElementById("palabraAdivinar").innerHTML = adivinanza;
 
 function jugarLetra(letra) {
-    let desglocePalabra = palabraAdivinar.split("");
-    let desgloceAdivinanza = adivinanza.split("");
-    
-    for (let i = 0; i < palabraAdivinar.length; i++) {
-        if (desglocePalabra[i] == letra.toUpperCase() || desglocePalabra[i] == letra.toLowerCase()) {
-            desgloceAdivinanza[i] = palabraAdivinar.charAt(i);
+    if (adivinanza != palabraAdivinar) {
+        let desglocePalabra = palabraAdivinar.split("");
+        let desgloceAdivinanza = adivinanza.split("");
+
+        for (let i = 0; i < palabraAdivinar.length; i++) {
+            if (desglocePalabra[i] == letra.toUpperCase() || desglocePalabra[i] == letra.toLowerCase()) {
+                desgloceAdivinanza[i] = palabraAdivinar.charAt(i);
+            }
         }
+        document.getElementById(letra).disabled = true;
+
+        adivinanza = desgloceAdivinanza.join("");
+
+        comprobarVictoria(letra);
+
+        document.getElementById("palabraAdivinar").innerHTML = adivinanza;
     }
-
-    if (palabraAdivinar.includes(letra.toUpperCase()) || palabraAdivinar.includes(letra.toLowerCase())) {
-        document.getElementById(letra).classList.add("hidden")
-    }
-
-    adivinanza = desgloceAdivinanza.join("");
-
-    comprobarVictoria();
-
-    document.getElementById("palabraAdivinar").innerHTML = adivinanza;
 }
 
-function comprobarVictoria() {
+function comprobarVictoria(letra) {
+    let divAvance = document.getElementById("avance");
+
     if (adivinanza == palabraAdivinar) {
-        alert("Has ganao bro!");
+        divAvance.innerHTML = "🥰 ¡Has ganado! 😍";
+        terminarPartida();
+        document.getElementById("btnNuevaPartida").classList.remove("hidden");
+    } else if (intentos > 0 && !(palabraAdivinar.includes(letra.toUpperCase()) || palabraAdivinar.includes(letra.toLowerCase()))) {
+        divAvance.innerHTML = "Te quedan " + intentos + " intentos.";
+        intentos--;
+    } else if (intentos == 0) {
+        divAvance.innerHTML = "☹️ ¡Has perdido! ☹️";
+        terminarPartida();
+        document.getElementById("btnNuevaPartida").classList.remove("hidden");
     }
+}
+
+function terminarPartida() {
+    const letras = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ";
+
+    for (let i = 0; i < letras.length; i++) {
+        document.getElementById(letras.charAt(i)).disabled = true;
+    }
+}
+
+function nuevaPartida() {
+    window.location.reload();
 }
